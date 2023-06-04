25d6bd2b85b9211c642b66cd13408a49127523cd
Alrien <33385948+nietzschesimp@users.noreply.github.com>
2023-06-03 21:59:09 -0400

    Remove unnecessary dependency

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -2,7 +2,6 @@
 
 pub mod csv;
 
-use simple_error::bail;
 use lazy_static::lazy_static;
 
 use crate::data::data_frame::DataFrame;
@@ -21,7 +20,7 @@ lazy_static!{
 
 pub fn read_input(address: &str, format: &str, missing_values: Vec<&str>, headers: bool) -> Result<DataFrame<Option<String>>, Box<dyn Error>>{
     if !INPUT_FORMAT_MAP.contains_key(format) {
-        bail!("Invalid input format!");
+        return Err("Invalid input format!".into());
     }
     INPUT_FORMAT_MAP[format].read(address, &missing_values, headers)
 }
