1329cb65d8b45f7e99f1f3e1960bf2327ce338ed
Alrien <33385948+nietzschesimp@users.noreply.github.com>
2023-06-03 22:45:00 -0400

    uses rustfmt

--- src/models/null.rs
+++ src/models/null.rs
@@ -12,17 +12,21 @@ pub struct NullModel {
 impl Model for NullModel {
     fn from_training(_training_values: &[&[Numeric]], target_values: &[Numeric]) -> Self {
         let mut value_count = HashMap::new();
-        
+
         for &value in target_values {
             let counter = value_count.entry(value as i64).or_insert(0);
             *counter += 1;
         }
-        
+
         Self {
-            value_to_replace: value_count.iter().max_by_key(|&(_, count)| count).map(|(val, _)| val).expect("Mode not found!")
+            value_to_replace: value_count
+                .iter()
+                .max_by_key(|&(_, count)| count)
+                .map(|(val, _)| val)
+                .expect("Mode not found!"),
         }
     }
-    
+
     fn predict(&self, sample: &[Numeric]) -> Result<Numeric, Box<dyn Error>> {
         Ok(self.value_to_replace)
     }
