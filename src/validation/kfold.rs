19c753f955b1ca93746c1b19e8421a0bdcee47f9
Alrien <33385948+nietzschesimp@users.noreply.github.com>
2023-06-10 11:30:43 -0400

    renaming class to label

--- src/validation/kfold.rs
+++ src/validation/kfold.rs
@@ -13,39 +13,38 @@ pub struct KFold;
 impl KFold {
     pub fn partition(
         table: &DataFrame<Numeric>,
-        target_idx: usize,
+        label_column_idx: usize,
         k: usize,
     ) -> Result<Vec<(Vec<usize>, Vec<usize>)>, Box<dyn Error>> {
-        if table.get_column_idx(target_idx).is_none() {
+        if table.get_column_idx(label_column_idx).is_none() {
             return Err("Couldn't find index of column of target value!".into());
         }
 
-        let target_values = table.get_column_idx(target_idx).unwrap();
-        let num_samples = target_values.values().len();
+        let label_column = table.get_column_idx(label_column_idx).unwrap();
+        let num_samples = label_column.values().len();
         let fold_size = num_samples / k;
 
         // Shuffle indexes
-        let mut class_indices = (0..num_samples).collect_vec();
-        class_indices.shuffle(&mut thread_rng());
+        let mut indexes = (0..num_samples).collect_vec();
+        indexes.shuffle(&mut thread_rng());
 
         // Generate indeces for k - 1 folds
         let mut ret = Vec::with_capacity(k);
         for idx in 0..k - 1 {
-            println!("FOLD: {}", idx + 1);
             let start = idx * fold_size;
             let end = (idx + 1) * fold_size;
             ret.push((
-                [&class_indices[0..start], &class_indices[end..]].concat(),
-                class_indices[start..end].to_vec(),
+                [&indexes[0..start], &indexes[end..]].concat(),
+                indexes[start..end].to_vec(),
             ));
         }
 
-        // Generate indeces for fold k, making the fold size a bit larger using a remainder
+        // Generate indeces for last fold. Also making the fold use the remainder values.
         let fold_size = (num_samples / k) + (num_samples % k);
         let start = num_samples - fold_size;
         ret.push((
-            class_indices[0..start].to_vec(),
-            class_indices[start..].to_vec(),
+            indexes[0..start].to_vec(),
+            indexes[start..].to_vec(),
         ));
 
         Ok(ret)
--- src/validation/kfold_stratified.rs
+++ src/validation/kfold_stratified.rs
@@ -12,33 +12,33 @@ pub struct StratifiedKFold;
 impl StratifiedKFold {
     pub fn partition(
         table: &DataFrame<Numeric>,
-        target_idx: usize,
+        label_column_idx: usize,
         k: usize,
     ) -> Result<Vec<(Vec<usize>, Vec<usize>)>, Box<dyn Error>> {
-        if table.get_column_idx(target_idx).is_none() {
+        if table.get_column_idx(label_column_idx).is_none() {
             return Err("Couldn't find index of column of target value!".into());
         }
 
-        let target_values = table.get_column_idx(target_idx).unwrap();
+        let label_column = table.get_column_idx(label_column_idx).unwrap();
 
         // Populate set of class entries with indexes
-        let mut class_indices: HashMap<i64, Vec<usize>> = HashMap::new();
-        for (idx, &value) in target_values.values().enumerate() {
-            let class_list = class_indices.entry(value as i64).or_insert(vec![]);
-            class_list.push(idx);
+        let mut label_indices: HashMap<i64, Vec<usize>> = HashMap::new();
+        for (idx, &value) in label_column.values().enumerate() {
+            let index_list = label_indices.entry(value as i64).or_insert(vec![]);
+            index_list.push(idx);
         }
 
         // Shuffle indexes in each vector
-        class_indices
+        label_indices
             .values_mut()
             .for_each(|indices| indices.shuffle(&mut thread_rng()));
 
-        for (category, class_list) in class_indices.iter() {
+        for (label, index_list) in label_indices.iter() {
             println!(
-                "cat: {} | n: {} | p: {}",
-                category,
-                class_list.len(),
-                100.0 * class_list.len() as f32 / target_values.values().len() as f32
+                "label: {} | n: {} | p: {}",
+                label,
+                index_list.len(),
+                100.0 * index_list.len() as f32 / label_column.values().len() as f32
             );
         }
 
@@ -48,21 +48,21 @@ impl StratifiedKFold {
             println!("FOLD: {}", idx + 1);
             let mut validation_indices = Vec::new();
             let mut training_indices = Vec::new();
-            let mut class_sizes = Vec::new();
-            for (_class_idx, indexes) in class_indices.iter() {
+            let mut label_sizes = Vec::new();
+            for indexes in label_indices.values() {
                 let fold_size = indexes.len() / k;
                 let start = idx * fold_size;
                 let end = (idx + 1) * fold_size;
                 validation_indices.extend(indexes[start..end].to_vec());
                 training_indices.extend([&indexes[0..start], &indexes[end..]].concat());
-                class_sizes.push(indexes[0..start].len() + indexes[end..].len());
+                label_sizes.push(indexes[0..start].len() + indexes[end..].len());
             }
-            for (class_idx, class_size) in class_sizes.into_iter().enumerate() {
+            for (label, label_size) in label_sizes.into_iter().enumerate() {
                 println!(
-                    "cat: {} | n: {} | p: {}",
-                    class_idx,
-                    class_size,
-                    100.0 * class_size as f32 / training_indices.len() as f32
+                    "label: {} | n: {} | p: {}",
+                    label,
+                    label_size,
+                    100.0 * label_size as f32 / training_indices.len() as f32
                 );
             }
             ret.push((training_indices, validation_indices));
@@ -70,22 +70,22 @@ impl StratifiedKFold {
 
         let mut validation_indices = Vec::new();
         let mut training_indices = Vec::new();
-        let mut class_sizes = Vec::new();
+        let mut label_sizes = Vec::new();
         println!("FOLD: {}", k);
-        for (_class_idx, indexes) in class_indices.iter() {
-            let fold_size = (indexes.len() / k) + (indexes.len() % (indexes.len() / k));
+        for indexes in label_indices.values() {
+            let fold_size = (indexes.len() / k) + (indexes.len() % k);
             let end = indexes.len();
             let start = end - fold_size;
             validation_indices.extend(indexes[start..end].to_vec());
             training_indices.extend(indexes[0..start].to_vec());
-            class_sizes.push(indexes[0..start].len());
+            label_sizes.push(indexes[0..start].len());
         }
-        for (class_idx, class_size) in class_sizes.into_iter().enumerate() {
+        for (label, label_size) in label_sizes.into_iter().enumerate() {
             println!(
-                "cat: {} | n: {} | p: {}",
-                class_idx,
-                class_size,
-                100.0 * class_size as f32 / training_indices.len() as f32
+                "label: {} | n: {} | p: {}",
+                label,
+                label_size,
+                100.0 * label_size as f32 / training_indices.len() as f32
             );
         }
         ret.push((training_indices, validation_indices));
