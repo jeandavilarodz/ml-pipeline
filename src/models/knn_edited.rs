8401873fa14df720cf46b0362ec36938aa90df47
Nietzschesimp <33385948+nietzschesimp@users.noreply.github.com>
2023-06-18 22:54:20 -0400

    Initial edited KNN impl

--- src/models/knn_edited.rs
+++ src/models/knn_edited.rs
@@ -11,12 +11,11 @@ use crate::types::Numeric;
 use std::collections::HashMap;
 use std::error::Error;
 
-use num_traits::ToPrimitive;
-
 pub struct EditedKNearestNeighborTrainer {
     training_data: Option<Vec<Box<[Numeric]>>>,
     num_neighbors: Option<usize>,
     label_index: Option<usize>,
+    epsilon: f64,
     model_snapshot: Vec<Box<[Numeric]>>,
 }
 
@@ -29,6 +28,7 @@ impl ModelTrainer for EditedKNearestNeighborTrainer {
             num_neighbors: None,
             training_data: None,
             label_index: None,
+            epsilon:  1e-8,
             model_snapshot: vec![],
         }
     }
@@ -38,12 +38,9 @@ impl ModelTrainer for EditedKNearestNeighborTrainer {
         parameters: &Option<HashMap<String, Numeric>>,
     ) -> Result<(), Box<dyn Error>> {
         let parameters = parameters.as_ref().ok_or("No parameters given!")?;
-        let num_neighbors = parameters
-            .get("num_neighbors")
-            .ok_or("num_neighbors parameter not present!")?
-            .to_usize()
-            .ok_or("Could not parse num_neighbors as usize!")?;
-        self.num_neighbors = Some(num_neighbors);
+        self.epsilon = *parameters
+            .get("epsilon")
+            .unwrap_or(&1e-8);
         Ok(())
     }
 
@@ -64,21 +61,24 @@ impl ModelTrainer for EditedKNearestNeighborTrainer {
     }
 
     fn train(&mut self) -> Result<Box<dyn Model>, Box<dyn Error>> {
+        let training_data = self.training_data.as_mut().ok_or("No training data!")?;
+        training_data.extend(self.model_snapshot.iter().cloned());
+
         let mut model = KNearestNeighbor {
             num_neighbors: self.num_neighbors.ok_or("no num_neighbors")?,
             label_index: self.label_index.ok_or("no label_index")?,
-            label_examples: self.model_snapshot.clone(),
+            label_examples: training_data.clone(),
         };
 
         // Predict values and if the label doesn't match add the input value to the set
-        for sample in self.training_data.as_ref().ok_or("No training data!")?.into_iter() {
+        for (idx, sample) in training_data.iter().enumerate().rev() {
             let prediction = model.predict(sample.clone());
             if (prediction - sample[model.label_index]).abs() < 1e-8 {
-                self.model_snapshot.push(sample.clone());
+                model.label_examples.remove(idx);
             }
         }
 
-        model.label_examples = self.model_snapshot.clone();
+        self.model_snapshot = model.label_examples.clone();
 
         Ok(Box::new(model))
     }
