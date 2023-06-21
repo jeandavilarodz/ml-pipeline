c6b27b4d4ac86cb60569c38826d1eba2d3afada8
Nietzschesimp <33385948+nietzschesimp@users.noreply.github.com>
2023-06-20 21:50:38 -0400

    added comments

--- src/models/knn_classifier.rs
+++ src/models/knn_classifier.rs
@@ -16,6 +16,7 @@ pub struct KNearestNeighbor {
 
 impl Model for KNearestNeighbor {
     fn predict(&self, sample: Box<[Numeric]>) -> Numeric {
+        // Calculate distances between each example and the k nearest neighbors
         let mut distances = Vec::new();
         for (index, training_sample) in self.label_examples.iter().enumerate() {
             distances.push((
@@ -23,8 +24,10 @@ impl Model for KNearestNeighbor {
                 euclidean_distance(training_sample.clone(), sample.clone()),
             ));
         }
+        // Sort the distances by distance
         distances.sort_by(|(_, x), (_, y)| x.abs().partial_cmp(&y.abs()).unwrap());
 
+        // Get the label count of the k nearest neighbors
         let mut label_count = HashMap::new();
         for (idx, _) in distances[..self.num_neighbors].into_iter() {
             let label = self.label_examples[*idx][self.label_index];
@@ -33,20 +36,25 @@ impl Model for KNearestNeighbor {
             *counter += 1;
         }
 
+        // Get the most common label
         let mode = label_count
             .iter()
             .max_by_key(|&(_, count)| count)
             .map(|(val, _)| val)
             .expect("No mode found!");
 
+        // return the most common label
         (*mode as f64) * 1e-8
     }
 }
 
 fn euclidean_distance(row1: Box<[Numeric]>, row2: Box<[Numeric]>) -> Numeric {
+    // Calculate the euclidean distance between the two rows
     let distance = row1
         .iter()
         .zip(row2.iter())
         .fold(0.0, |acc, (&e1, &e2)| acc + (e1 - e2).powi(2));
+
+    // Return the distance
     distance.sqrt()
 }
