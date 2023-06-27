7da6d457414a4562c2a65e62902a41088d8eedbe
Nietzschesimp <33385948+nietzschesimp@users.noreply.github.com>
2023-06-26 21:21:37 -0400

    Using training vlaue variance to determine gamma

--- src/models/knn_condensed.rs
+++ src/models/knn_condensed.rs
@@ -57,7 +57,18 @@ impl ModelBuilder for CondensedKNearestNeighborTrainer {
         // Build examples for the algorithm
         let mut label_examples = Vec::new();
 
+        // Push first sample of training data
         label_examples.push(training_data.next().unwrap().clone());
+
+        // Calculate training value mean
+        let training_value_mean = training_values.iter().fold(0.0, |acc, x| {
+            acc + x[target_value_idx]
+        }) / training_values.len() as f64;
+
+        // Calculate training value variance
+        let training_value_variance = training_values.iter().fold(0.0, |acc, sample| {
+            acc + (sample[target_value_idx] - training_value_mean).powi(2)
+        }) / (training_values.len() - 1) as f64;
 
         // Build k-nearest neighbors model with the label examples
         let mut model = KNearestNeighbor {
@@ -65,7 +76,7 @@ impl ModelBuilder for CondensedKNearestNeighborTrainer {
             label_index: target_value_idx,
             label_examples,
             epsilon: self.epsilon,
-            gamma: 1.0,
+            gamma: training_value_variance.sqrt(),
         };
 
         if let Some(hyperparameters) = self.hyperparameters.as_ref() {
